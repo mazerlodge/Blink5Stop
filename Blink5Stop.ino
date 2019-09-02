1dce18217d0d241f9793317bced762102f72e082
mazerlodge <mazerlodge@hotmail.com>
2019-09-02 10:09:01 -0500

    Set codebase for nano board

--- Blink5Stop.ino
+++ Blink5Stop.ino
@@ -9,34 +9,32 @@
 // Variable used to shut off blinking cycle.
 boolean bDone;
 
+// Pin 13 has an LED connected on most Arduino boards.
+// Pin 0 on Feather board.
+int LED_PIN = 13; 
+
 void setup() {                
   // initialize the digital pin as an output.
-  // Pin 13 has an LED connected on most Arduino boards:
-  // Pin 0 on Feather board.
-  pinMode(0, OUTPUT);     
+  pinMode(LED_PIN, OUTPUT);     
 
   // Start with blinking enabled.
   bDone = false;
 
-  // Enable debug printing
-  Serial.begin(115200);
-
-  Serial.println("Setup complete.");
 }
 
 void loop() {
 
   if (!bDone) {
     for (int x=0; x<7; x++) {
-      digitalWrite(0, LOW);   // set the LED on
+      digitalWrite(LED_PIN, HIGH);   // set the LED on
       delay(500);              // wait for a second
-      digitalWrite(0, HIGH);    // set the LED off
+      digitalWrite(LED_PIN, LOW);    // set the LED off
       delay(500);              // wait for a second
-
     } // for x
 
     // stop blinking
     bDone = true;
+
       
   } // if !bDone
     
